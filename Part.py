b095cd859095aead8c7532c1ef4866db7c0f567c
allan <allantsai3@gmail.com>
2021-04-06 21:54:37 -0700

    Initial commit for mixer that randomly chooses chair parts and merges into obj file.

---
+++ Part.py
@@ -0,0 +1,57 @@
+import os
+"""
+This file contains the definition for a part
+"""
+
+class Part(object):
+    def __init__(self, file_list, obj_file_loc):
+        self.vertices = []
+        self.faces = []
+
+        self.load_part(file_list, obj_file_loc)
+        # Get center of mass after loading all parts
+        self.get_center_of_mass(self.vertices, self.faces)
+
+    def get_vertices(self):
+        return self.vertices
+
+    def get_faces(self):
+        return self.faces
+
+    def get_center_of_mass(self, vertices, faces):
+        pass
+
+    def load_part(self, file_list, obj_file_loc):
+        for file in file_list:
+            ## Extracted from https://inareous.github.io/posts/opening-obj-using-py
+            try:
+                f = open(os.path.join(obj_file_loc, file + ".obj"))
+                for line in f:
+                    if line[:2] == "v ":
+                        index1 = line.find(" ") + 1
+                        index2 = line.find(" ", index1 + 1)
+                        index3 = line.find(" ", index2 + 1)
+
+                        vertex = (float(line[index1:index2]), float(line[index2:index3]), float(line[index3:-1]))
+                        # vertex = (round(vertex[0], 2), round(vertex[1], 2), round(vertex[2], 2))
+                        self.vertices.append(vertex)
+
+                    elif line[0] == "f":
+                        string = line.replace("//", "/")
+                        ##
+                        i = string.find(" ") + 1
+                        face = []
+                        for item in range(string.count(" ")):
+                            if string.find(" ", i) == -1:
+                                face.append(string[i:-1])
+                                break
+                            face.append(string[i:string.find(" ", i)])
+                            i = string.find(" ", i) + 1
+                        ##
+                        self.faces.append(tuple(face))
+
+                f.close()
+            except IOError:
+                print(".obj file not found")
+
+
---
+++ mixer.py
@@ -0,0 +1,80 @@
+import sys
+import os
+import random
+import json
+from Part import Part
+"""
+Axioms for chair:
+ - Something that can be sit on (has a base)
+ - center of balance/ stable (unless rocking chair etc..)
+"""
+
+"""
+    Input: set of 3D parts and labels
+    Output: Generated 3D model (and also orthogonal projection in multiple views)
+"""
+
+
+def reconstruct_obj(part_list):
+    # Create the output directory if it doesn't exist
+    directory = "tmp_output"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    f = open("./tmp_output/output_mesh.obj", "w")
+
+    for num, part in enumerate(part_list):
+        f.write("g " + str(num) + "\n")
+        vertices = part.get_vertices()
+        for vertex in vertices:
+            f.write("v " + str(vertex[0]) + " " + str(vertex[1]) + " " + str(vertex[2]) + "\n")
+        faces = part.get_faces()
+        for face in faces:
+            f.write("f " + str(face[0]) + " " + str(face[1]) + " " + str(face[2]) + "\n")
+
+
+if __name__ == "__main__":
+    dataDir = sys.argv[1]
+
+    sub_folders = [name for name in os.listdir(dataDir) if os.path.isdir(os.path.join(dataDir, name))]
+
+    random.seed(23)
+    index = random.choices(range(len(sub_folders)), k=4)
+
+    chosenParts = [sub_folders[part] for part in index]
+
+    print("Chosen parts are from folder:")
+    print(chosenParts)
+
+    partsList = []
+
+    # Choose the chair_seat, chair_back, chair_base, chair_arm
+    for i, part in enumerate(chosenParts):
+        print(i)
+        with open(os.path.join(dataDir, part, "result_after_merging.json")) as json_file:
+            try:
+                data = json.load(json_file)
+            except ValueError:
+                print(part)
+            except IOError:
+                print('IOError')
+
+            obj_Files = os.path.join(dataDir, part, "objs")
+
+            for obj in data[0]['children']:
+                partName = obj['name']
+                if partName == 'chair_seat' and i == 0:
+                    partsList.append(Part(obj["objs"], obj_Files))
+
+                if partName == 'chair_back' and i == 1:
+                    partsList.append(Part(obj["objs"], obj_Files))
+
+                if partName == 'chair_base' and i == 2:
+                    partsList.append(Part(obj["objs"], obj_Files))
+
+                if partName == 'chair_arm' and i == 3:
+                    partsList.append(Part(obj["objs"], obj_Files))
+
+
+    # Combine the various part vertices/faces and then output obj files
+    reconstruct_obj(partsList)
+
